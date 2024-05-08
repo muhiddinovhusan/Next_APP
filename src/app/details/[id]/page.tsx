44e51230bcea7f60bc561edbc7920eb4f1c3bb82
XXX <muhiddinovhusan0207@gmail.com>
2024-05-08 17:22:37 +0500

    x

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div className='flex w-full  max-md:flex-col' >
-     <Link href="/">
+   <Link href='/'>
+   <h2 className='ml-12'>back</h2>
 
-      <h2 className='ml-12'>back</h2>
-      <Link/>
+   </Link>
 <div className='w-[50%]  max-md:w-full p-8 max-md:p-12'>
 <img className='w-full rounded-md' src={germany.src} alt="" />
 </div>
